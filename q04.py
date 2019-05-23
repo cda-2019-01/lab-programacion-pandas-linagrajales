1352ea2cce502c7f0fd3be32b1b2671650d0340b
Lina María Grajales V <50255623+linagrajales@users.noreply.github.com>
2019-05-23 10:01:26 -0500

    Update q04.py

--- q04.py
+++ q04.py
@@ -2,5 +2,9 @@
 ## Imprima los valores unicos e la columna _c4 de 
 ## de la tabla tbl1 en mayusculas
 ## 
-
-
+import pandas as pd
+import numpy as np
+import itertools
+data = pd.read_csv("tbl0.tsv", sep="\t")
+c = sorted(data1['_c4'].unique())
+print(str(c).upper())
