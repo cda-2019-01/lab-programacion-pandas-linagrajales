0fb0e3ac5e24a1bc90bc407c5c9f0a3deadaaab7
Lina María Grajales V <50255623+linagrajales@users.noreply.github.com>
2019-05-24 09:10:06 -0500

    Update q10.py

--- q10.py
+++ q10.py
@@ -3,4 +3,13 @@
 ## separada por ',' de los valores de la columna _c4
 ## de la tabla tbl1.tsv
 ## 
-
+data1 = pd.read_csv("tbl1.tsv", sep="\t")
+c0 = sorted(data1['_c0'].unique())
+lista = []
+for val in c0:
+    df = (data1[data1['_c0'] == val])
+    df1 = sorted(df['_c4'].tolist())
+    df2 = ",".join(map (str, df1))
+    lista.append(df2)
+out = pd.DataFrame({"_c0":c0,"lista":lista})
+print(out)
