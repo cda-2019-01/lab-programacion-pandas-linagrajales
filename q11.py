23521c6f3381c434f6fd30c7ca451940ea985d66
Lina María Grajales V <50255623+linagrajales@users.noreply.github.com>
2019-05-23 22:55:34 -0500

    Update q11.py

--- q11.py
+++ q11.py
@@ -3,4 +3,16 @@
 ## separada por ',' de los valores de la columna _c5a
 ## y _c5b (unidos por ':') de la tabla tbl2.tsv
 ## 
-
+import pandas as pd
+import numpy as np
+data2 = pd.read_csv("tbl2.tsv", sep="\t")
+data2['_c']=data2['_c5a']+":"+data2['_c5b'].astype(str)
+c0 = sorted(data2['_c0'].unique())
+lista = []
+for val in c0:
+    df = (data2[data2['_c0'] == val])
+    df1 = sorted(df['_c'].tolist())
+    df2 = ",".join(map (str, df1))
+    lista.append(df2)
+out = pd.DataFrame({"_c0":c0,"lista":lista})
+print(out)
