65970a00fc71666bd80032de8677ff6491d147ff
Lina María Grajales V <50255623+linagrajales@users.noreply.github.com>
2019-05-24 09:08:00 -0500

    Update q09.py

--- q09.py
+++ q09.py
@@ -3,5 +3,16 @@
 ## separada por ':' de los valores de la columna _c2
 ## para el archivo tbl0.tsv
 ## 
-
-
+import pandas as pd
+import numpy as np
+import itertools
+data = pd.read_csv("tbl0.tsv", sep="\t")
+c1 = sorted(data['_c1'].unique())
+lista = []
+for val in c1:
+    df = (data[data['_c1'] == val])
+    df1 = sorted(df['_c2'].tolist())
+    df2 = ":".join(map (str, df1))
+    lista.append(df2)
+out = pd.DataFrame({"_c0":c1,"lista":lista})
+print(out)
